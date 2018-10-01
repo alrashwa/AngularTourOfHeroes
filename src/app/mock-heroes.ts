8e1e938503f71f05f4901129a10e785d06ec1098
alrashwa <alrashwa@yahoo.com>
2018-10-01 00:03:52 -0700

    Completed tutorial step 4, Displaying List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'The Zuckler'},
+    { id: 12, name: 'Bezos the Clown'},
+    { id: 13, name: 'Musket'},
+    { id: 14, name: 'Super Koch Bros.'},
+    { id: 15, name: 'Fiber Man'},
+    { id: 16, name: 'Insulin Boy'},
+    { id: 17, name: 'Anti-Vaxxen'},
+    { id: 18, name: 'Tinfoil Pants Man'},
+    { id: 19, name: 'SuperMon'},
+    { id: 20, name: 'Mor Man'}
+];
